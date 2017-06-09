e74e32ed0ad3f4558ac69c93e1d930a72ab15c5d
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-06-09 18:02:04 +0700

    update socketRest client

--- src/main/java/client/example.java
+++ src/main/java/client/example.java
@@ -2,11 +2,14 @@ package client;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -17,24 +20,40 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 public class example {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		HttpClient client = HttpClientBuilder.create().build();
-		File file = new File("C:\\Users\\Ratata1590\\Downloads\\site.zip");
-		HttpPost post = new HttpPost("http://localhost:8080/dynamicCodeRest/uploadFile");
-		FileBody fileBody = new FileBody(file);
-		StringBody stringBody1 = new StringBody("C:\\Users\\Ratata1590\\Desktop\\New folder\\",
-				ContentType.MULTIPART_FORM_DATA);
-		InputStreamBody uploadFilePart = new InputStreamBody(new FileInputStream(file),
-				ContentType.APPLICATION_OCTET_STREAM);
-		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-		builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-		builder.addPart("file", fileBody);
-		builder.addPart("path", stringBody1);
-		HttpEntity entity = builder.build();
-		//
-		post.setEntity(entity);
-		HttpResponse response = client.execute(post);
-	}
+  public static void main(String[] args) throws Exception {
+
+    ServerSocket serverSocket = new ServerSocket(1521);
+    Socket clientSocket = serverSocket.accept();
+    CleanUpThread recv = new CleanUpThread();
+    recv.setClientSocket(clientSocket);
+    byte[] tmpbuff = new byte[0];
+    while (true) {
+      // TODO Auto-generated method stub
+      HttpClient client = HttpClientBuilder.create().build();
+      // File file = new File("C:\\Users\\Ratata1590\\Downloads\\site.zip");
+      HttpPost post = new HttpPost("http://localhost:8080/dynamicCodeRest/socketHandlerUp");
+      // FileBody fileBody = new FileBody(file);
+
+      // MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+      // builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+      // builder.addPart("file", fileBody);
+      // builder.addPart("path", stringBody1);
+      // HttpEntity entity = builder.build();
+      //
+      byte[] resultBuff = new byte[clientSocket.getReceiveBufferSize()];
+      tmpbuff = new byte[clientSocket.getInputStream().read(resultBuff, 0, resultBuff.length)];
+      System.arraycopy(resultBuff, 0, tmpbuff, 0, tmpbuff.length);
+      String data = new String(tmpbuff);
+      System.out.println(data);
+
+      post.setEntity(new ByteArrayEntity(tmpbuff));
+      client.execute(post);
+
+      // response.getEntity().writeTo(clientSocket.getOutputStream());
+      // clientSocket.getOutputStream().flush();
+
+
+    }
+  }
 
 }
