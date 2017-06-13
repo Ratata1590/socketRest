fe746dfb28e2b5a9bf4970c7c753aa00d936a31f
Ratata1590 <Ratata1590@Ratata1590-PC>
2017-06-13 07:15:15 +0700

    fix some stupid things ^^!

--- src/main/java/com/ratata/socketRestServer/Threads/RECV.java
+++ src/main/java/com/ratata/socketRestServer/Threads/RECV.java
@@ -6,32 +6,32 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class RECV extends LinkAbstract {
-  public void run() {
-    while (!Thread.currentThread().isInterrupted()) {
-      HttpClient client = HttpClientBuilder.create().build();
-      HttpGet get = new HttpGet(this.proxyUrl.concat("/socketHandler"));
-      get.addHeader("sockRestId", sessionId.toString());
-      try {
-        HttpResponse response;
-        int ctry = retry;
-        do {
-          response = client.execute(get);
-          ctry--;
-          if (ctry == 0) {
-            throw new Exception("out of retry");
-          }
-        } while (response.getStatusLine().getStatusCode() != 200);
-        response.getEntity().writeTo(sock.getOutputStream());
-        sock.getOutputStream().flush();
-      } catch (Exception e) {
-        disconnectRemoteSocket();
-        try {
+	public void run() {
+		while (!Thread.currentThread().isInterrupted()) {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpGet get = new HttpGet(this.proxyUrl.concat("/socketHandler"));
+			get.addHeader("sockRestId", sessionId);
+			try {
+				HttpResponse response;
+				int ctry = retry;
+				do {
+					response = client.execute(get);
+					ctry--;
+					if (ctry == 0) {
+						throw new Exception("out of retry");
+					}
+				} while (response.getStatusLine().getStatusCode() != 200);
+				response.getEntity().writeTo(sock.getOutputStream());
+				sock.getOutputStream().flush();
+			} catch (Exception e) {
+				disconnectRemoteSocket();
+				try {
 
-          sock.close();
-        } catch (Exception e2) {
-        }
-        break;
-      }
-    }
-  }
+					sock.close();
+				} catch (Exception e2) {
+				}
+				break;
+			}
+		}
+	}
 }
--- src/main/java/com/ratata/socketRestServer/Threads/SEND.java
+++ src/main/java/com/ratata/socketRestServer/Threads/SEND.java
@@ -8,36 +8,36 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 public class SEND extends LinkAbstract {
 
-  public void run() {
-    while (!Thread.currentThread().isInterrupted()) {
-      HttpClient client = HttpClientBuilder.create().build();
-      HttpPost post = new HttpPost(proxyUrl.concat("/socketHandler"));
-      post.addHeader("sockRestId", sessionId.toString());
-      byte[] tmpbuff;
-      byte[] resultBuff;
-      try {
-        resultBuff = new byte[sock.getReceiveBufferSize()];
-        tmpbuff = new byte[sock.getInputStream().read(resultBuff, 0, resultBuff.length)];
-        System.arraycopy(resultBuff, 0, tmpbuff, 0, tmpbuff.length);
-        post.setEntity(new ByteArrayEntity(tmpbuff));
+	public void run() {
+		while (!Thread.currentThread().isInterrupted()) {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpPost post = new HttpPost(proxyUrl.concat("/socketHandler"));
+			post.addHeader("sockRestId", sessionId);
+			byte[] tmpbuff;
+			byte[] resultBuff;
+			try {
+				resultBuff = new byte[sock.getReceiveBufferSize()];
+				tmpbuff = new byte[sock.getInputStream().read(resultBuff, 0, resultBuff.length)];
+				System.arraycopy(resultBuff, 0, tmpbuff, 0, tmpbuff.length);
+				post.setEntity(new ByteArrayEntity(tmpbuff));
 
-        HttpResponse response;
-        int ctry = retry;
-        do {
-          response = client.execute(post);
-          ctry--;
-          if (ctry == 0) {
-            throw new Exception("out of retry");
-          }
-        } while (response.getStatusLine().getStatusCode() != 200);
-      } catch (Exception e) {
-        disconnectRemoteSocket();
-        try {
-          sock.close();
-        } catch (Exception e2) {
-        }
-        break;
-      }
-    }
-  }
+				HttpResponse response;
+				int ctry = retry;
+				do {
+					response = client.execute(post);
+					ctry--;
+					if (ctry == 0) {
+						throw new Exception("out of retry");
+					}
+				} while (response.getStatusLine().getStatusCode() != 200);
+			} catch (Exception e) {
+				disconnectRemoteSocket();
+				try {
+					sock.close();
+				} catch (Exception e2) {
+				}
+				break;
+			}
+		}
+	}
 }
